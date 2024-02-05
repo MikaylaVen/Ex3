12bae95a39ee9eba27b39b342a6d87cdad28dbe2
MikaylaVen <97713262+MikaylaVen@users.noreply.github.com>
2024-02-05 14:09:55 -0500

    Created quote app

---
+++ index.js
@@ -0,0 +1,16 @@
+
+//creating array of quotes
+const quotations = [
+    "The purpose of our lives is to be happy.-Dalai Lama",
+    "Never let the fear of striking out keep you from playing the game.-Babe Ruth ",
+    "Only a life lived for others is a life worthwhile.-Albert Einstein " 
+];
+//function to generate random quote
+const Getquote = () => {
+    //generating random index
+const randomIndex = [Math.floor(Math.random() * quotations.length)];
+//Returning random quote
+return quotations[randomIndex];
+};
+//Print a random quote to console
+console.log(Getquote());
